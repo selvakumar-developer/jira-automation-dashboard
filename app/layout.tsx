496a3c194df23c93180cba24f309bec91aba95f4
selvakumar-developer <selvatechy7@gmail.com>
2025-08-09 23:20:52 +0530

    feat: Update metadata for Jira Dashboard and streamline layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,23 @@
-import type { Metadata } from 'next'
-import Providers from './components/providers/Providers'
-import './globals.css'
+import type { Metadata } from "next";
+import Providers from "./components/providers/Providers";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "Jira Dashboard",
+  description: "Jira Dashboard",
+  generator: "Next.js",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>
-        <Providers>
-
-        {children}
-        </Providers>
-        </body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
-  )
+  );
 }
